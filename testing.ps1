836f0b5b9ed2e9ed1837073f978b4c1ff7bd2995
brianaddicks <brian@addicks.us>
2014-02-27 14:13:22 -0500

    added HelperGetPropertyMembers

---
+++ testing.ps1
@@ -0,0 +1,6 @@
+﻿$Device = "pegasus.addicks.us"
+$ApiKey = "LUFRPT1PR2JtSDl5M2tjTktBeTkyaGZMTURTTU9BZm89OFA0Rk1WMS8zZGtKN0FmVjRqQ0lxVHlRcmgvSVRoUnlzMW5OckNhVEZUZz0="
+ipmo C:\dev\poweralto2\poweralto2.psm1
+Get-PaDevice $Device $ApiKey
+$global:Test = new-object PowerAlto.SecurityRule
+$global:Test
