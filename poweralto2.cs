029dae50b9a876a77487254729310a54221cd909
brianaddicks <brian@addicks.us>
2014-03-03 09:09:59 -0500

    created zone object

--- poweralto2.cs
+++ poweralto2.cs
@@ -140,6 +140,34 @@ namespace PowerAlto {
 		
 	}
 	
+	public class Zone {
+		private string paNameMatch (string Name) {
+			string namePattern =  @"^[a-zA-Z0-9\-_\.]+$";
+			Regex nameRx = new Regex(namePattern);
+			Match nameMatch = nameRx.Match(Name);
+			if (nameMatch.Success) {
+				return Name;
+			} else {
+				throw new System.ArgumentException("Value can only contain alphanumeric, hyphens, underscores, or periods.");
+			}
+		}
+
+		private string name;
+		public string Name { get { return this.name; }
+												 set { this.name = paNameMatch( value ); } }
+		
+		public string ZoneType { get; set; } // validatset: tap, virtual wire layer2, layer3
+
+		public List<string> Interfaces { get; set; } //changing an interface zone will be done from the interface object
+
+		public string ZoneProtectionProfile { get; set; } //change to object once it's made
+		public string LogSetting { get; set; } //change to object once it's made
+		public bool EnableUserIdentification { get; set; }
+
+		public List<string> UserIdAclInclude { get; set; }
+		public List<string> UserIdAclExclude { get; set; }
+	}
+
 	public class AddressObject {
 
 		private string paNameMatch (string Name) {
@@ -199,6 +227,8 @@ namespace PowerAlto {
 
 		public List<string> Tags { get; set; }
 	}
+
+
 
 	public class SecurityRule {
 
