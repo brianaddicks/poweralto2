6bea699935d5b0167dff2c094216786460e1990c
brianaddicks <brian@addicks.us>
2014-03-03 15:28:50 -0500

    completed first pass of get-paaddressobject

--- poweralto2.psm1
+++ poweralto2.psm1
@@ -313,6 +313,70 @@ function Get-PaZone {
 }
 
 ###############################################################################
+
+function Get-PaAddressObject {
+    [CmdletBinding()]
+    Param (
+		[Parameter(Mandatory=$False,Position=0)]
+		[string]$Name,
+
+        [Parameter(Mandatory=$False)]
+        [switch]$Candidate
+    )
+
+    $Xpath = "/config/devices/entry/vsys/entry/address"
+
+    if ($Name) { $Xpath += "/entry[@name='$Name']" }
+
+    if ($Candidate) { $Action = "get"; Throw "not supported yet"  } `
+               else { $Action = "show" }
+    
+    $ResponseData = Get-PaConfig -Xpath $Xpath -Action $Action
+
+    Write-Verbose "Pulling configuration information from $($global:PaDeviceObject.Name)."
+    Write-Debug $ResponseData
+
+    if ($ResponseData.address) { $ResponseData = $ResponseData.address.entry } `
+                       else { $ResponseData = $ResponseData.entry             }
+
+    $ResponseTable = @()
+    foreach ($r in $ResponseData) {
+        $ResponseObject = New-Object PowerAlto.AddressObject
+        Write-Verbose "Creating new AddressObject"
+        
+        $ResponseObject.Name = $r.name
+        Write-Verbose "Setting Address Name $($r.name)"
+        
+        if ($r.'ip-netmask') {
+        #    $ResponseObject.AddressType = 'ip-netmask'
+            $ResponseObject.Address = $r.'ip-netmask'
+            Write-Verbose "Setting Address: ip-netmask/$($r.'ip-netmask')"
+        }
+
+        if ($r.'ip-range') {
+        #    $ResponseObject.AddressType = 'ip-range'
+            $ResponseObject.Address = $r.'ip-range'
+            Write-Verbose "Setting Address: ip-range/$($r.'ip-range')"
+        }
+
+        if ($r.fqdn) {
+        #    $ResponseObject.AddressType = 'fqdn'
+            $ResponseObject.Address = $r.fqdn
+            Write-Verbose "Setting Address: fqdn/$($r.fqdn)"
+        }
+
+        $ResponseObject.Tags = HelperGetPropertyMembers $r tag
+        $ResponseObject.Description = $r.description
+
+
+        $ResponseTable += $ResponseObject
+        Write-Verbose "Adding object to array"
+    }
+    
+    return $ResponseTable
+}
+
+###############################################################################
 ## Start Helper Functions
 ###############################################################################
 
