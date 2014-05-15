e4d71edd8632063b7cb1a9fc6fd25e69e0d36285
brianaddicks <brian@addicks.us>
2014-05-14 21:14:30 -0400

    split cs files and started on validators class

--- src/cs/AddressObject.cs
+++ src/cs/AddressObject.cs
@@ -1,74 +1,56 @@
-using System;
-using System.Net;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Web;
-using System.Xml;
-using System.Xml.Linq;
 using System.Text.RegularExpressions;
 
 namespace PowerAlto {
-	public class AddressObject {
+  public class AddressObject {
+  	Validator Validator = new Validator();
 
-		private string paNameMatch (string Name) {
-			string namePattern =  @"^[a-zA-Z0-9\-_\.]+$";
-			Regex nameRx = new Regex(namePattern);
-			Match nameMatch = nameRx.Match(Name);
-			if (nameMatch.Success) {
-				return Name;
-			} else {
-				throw new System.ArgumentException("Value can only contain alphanumeric, hyphens, underscores, or periods.");
-			}
-		}
+    private string name;
+    public string Name { get { return this.name; }
+                         set { this.name = Validator.AlphaNum( value ); } }
 
-		private string name;
-		public string Name { get { return this.name; }
-												 set { this.name = paNameMatch( value ); } }
+    public string Description { get; set; }
 
-		public string Description;
+    private string addressType;
+    public string AddressType { get { return this.addressType; } }
 
-		private string addressType;
-		public string AddressType { get { return this.addressType; } }
+    private string ipAddressMatch(string Input) {
+      string ipAddressMaskPattern = @"^\d+\.\d+\.\d+\.\d+(\/\d{2})?$"; //need better validation
+      Regex ipAddressMaskRx = new Regex( ipAddressMaskPattern );
+      Match ipAddressMaskMatch = ipAddressMaskRx.Match( Input );
 
-		private string ipAddressMatch (string Input) {
-			string ipAddressMaskPattern = @"^\d+\.\d+\.\d+\.\d+(\/\d{2})?$"; //need better validation
-			Regex ipAddressMaskRx = new Regex(ipAddressMaskPattern);
-			Match ipAddressMaskMatch = ipAddressMaskRx.Match(Input);
+      string ipRangePattern = @"^\d+\.\d+\.\d+\.\d+\-\d+\.\d+\.\d+\.\d+$";
+      Regex ipRangeRx = new Regex( ipRangePattern );
+      Match ipRangeMatch = ipRangeRx.Match( Input );
 
-			string ipRangePattern = @"^\d+\.\d+\.\d+\.\d+\-\d+\.\d+\.\d+\.\d+$";
-			Regex ipRangeRx = new Regex(ipRangePattern);
-			Match ipRangeMatch = ipRangeRx.Match(Input);
+      string fqdnPattern = @"^.+(\..+)+"; //needs better validation
+      Regex fqdnRx = new Regex( fqdnPattern );
+      Match fqdnMatch = fqdnRx.Match( Input );
 
-			string fqdnPattern = @"^.+(\..+)+"; //needs better validation
-			Regex fqdnRx = new Regex(fqdnPattern);
-			Match fqdnMatch = fqdnRx.Match(Input);
+      if ( ipAddressMaskMatch.Success ) {
+        this.addressType = "ip-netmask";
+        return Input;
+      }
 
-			if (ipAddressMaskMatch.Success) {
-				this.addressType = "ip-netmask";
-				return Input;
-			}
+      if ( ipRangeMatch.Success ) {
+        this.addressType = "ip-range";
+        return Input;
+      }
 
-			if (ipRangeMatch.Success) {
-				this.addressType = "ip-range";
-				return Input;
-			}
+      if ( fqdnMatch.Success ) {
+        this.addressType = "fqdn";
+        return Input;
+      }
 
-			if (fqdnMatch.Success) {
-				this.addressType = "fqdn";
-				return Input;
-			}
+      throw new System.ArgumentException( "Does not match" );
+    }
 
-			throw new System.ArgumentException("Does not match");
-		}
+    private string address;
+    public string Address {
+      get { return this.address; }
+      set { this.address = ipAddressMatch( value ); }
+    }
 
-		private string address;
-		public string Address { get { return this.address; }
-														set { this.address = ipAddressMatch( value ); } }
-
-		public List<string> Tags { get; set; }
-	}
+    public List<string> Tags { get; set; }
+  }
 }
--- src/cs/PaDevice.cs
+++ src/cs/PaDevice.cs
@@ -1,234 +1,217 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Web;
 using System.Xml;
-using System.Xml.Linq;
-using System.Text.RegularExpressions;
+using System.Web;
 
 namespace PowerAlto {
-/*
-	public class HelperClass {
-		public string paAlphaNum (string Name) {
-			string namePattern =  @"^[a-zA-Z0-9\-_\.]+$";
-			Regex nameRx = new Regex(namePattern);
-			Match nameMatch = nameRx.Match(Name);
-			if (nameMatch.Success) {
-				return Name;
-			} else {
-				throw new System.ArgumentException("Value can only contain alphanumeric, hyphens, underscores, or periods.");
-			}
-		}
-	}
-*/
-  public class HttpQueryReturnObject {
-    public HttpStatusCode Statuscode;
-    public string DetailedError;
-    public XmlDocument Data;
-    public string RawData;
-    public int HttpStatusCode {
-      get {
-        return (int)this.Statuscode;
-      }
-    }
-  }
-	public class PaDevice {
-		// can we rewrite the helper functions as methods in this class?
-		
-		private DateTime clock;
-		private Stack<string> urlhistory = new Stack<string>();
-		private string poweraltoversion = "2.0";
-		
-		public string Device { get; set; }
-		public int Port { get; set; }
-		public string ApiKey { get; set; }
-		public string Protocol { get; set; }
-		
-		public string Name { get; set; }
-		public string Model { get; set; }
-		public string Serial { get; set; }
-		
-		public string OsVersion { get; set; }
-		public string GpAgent { get; set; }
-		public string AppVersion { get; set; }
-		public string ThreatVersion { get; set; }
-		public string WildFireVersion { get; set; }
-		public string UrlVersion { get; set; }
-		public XmlDocument LastXmlResult { get; set; }
-		
-		public string ApiUrl {
-			get {
-				if (!string.IsNullOrEmpty(this.Protocol) && !string.IsNullOrEmpty(this.Device) && this.Port > 0) {
-					return this.Protocol + "://" + this.Device + ":" + this.Port + "/api/";
-				} else {
-					return null;
-				}
-			}
-		}
-		
-		public string AuthString {
-			get {
-				if (!string.IsNullOrEmpty(this.ApiKey)) {
-					return "key=" + this.ApiKey;
-				} else {
-					return null;
-				}
-			}
-		}
 
-	
-		public string PowerAltoVersion {
-			get {
-				return this.poweraltoversion;
-			}
-		}
-	
-		public string Clock {
-			get {
-				return this.clock.ToString();
-			}
-			set {
-				this.clock = DateTime.Parse(value);
-			}
-		}
-		public DateTime ClockasDateTime {
-			get {
-				return this.clock;
-			}
-			set {
-				this.clock = value;
-			}
-		}
-		
-		public string[] UrlHistory {
-			get {
-				return this.urlhistory.ToArray();
-			}
-		}
-		
-		public void FlushHistory () {
-			this.urlhistory.Clear();
-		}
-		
-		
-		public string UrlBuilder (string QueryString) {
-			
-			string[] Pieces = new string[5];
-			Pieces[0] = this.ApiUrl;
-			Pieces[1] = "?";
-			Pieces[2] = this.AuthString;
-			Pieces[3] = "&";
-			Pieces[4] = QueryString;
-			
-			//if (QueryType == "op") {
-			//	Pieces[5] += ("&cmd=" + Query);
-			//}
-			
-			string CompletedString = string.Join ("",Pieces);
-			
-			this.urlhistory.Push(CompletedString);
-			return CompletedString;
-		}
-				
-		private static bool OnValidateCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
-			return true;
-		}
-    
-		public void OverrideValidation() {
-			ServicePointManager.ServerCertificateValidationCallback = OnValidateCertificate;
-			ServicePointManager.Expect100Continue = true;
-			ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
-		}
-		
-		//Holds the raw result of the last query
-		//Would like to convert this to emulate UrlHistory, but I think we need to get the HttpQuery helper as a method to PaDevice first
-
-    private Stack<string> rawqueryhistory = new Stack<string>();
-
-    public string[] RawQueryHistory {
-      get {
-        return this.rawqueryhistory.ToArray();
-      }
-    }
-    
-    public void FlushRawQueryHistory () {
-      this.rawqueryhistory.Clear();
+    public class HttpQueryReturnObject {
+        public HttpStatusCode Statuscode;
+        public string DetailedError;
+        public XmlDocument Data;
+        public string RawData;
+        public int HttpStatusCode {
+            get {
+                return (int)this.Statuscode;
+            }
+        }
     }
 
-    /*
-    private Stack<XmlDocument> queryhistory = new Stack<XmlDocument>();
+    public class PaDevice {
+        // can we rewrite the helper functions as methods in this class?
 
-    public string[] QueryHistory {
-      get {
-        return this.queryhistory.ToArray();
-      }
-    }
-    
-    public void FlushQueryHistory () {
-      this.queryhistory.Clear();
-    }
-    */
+        private DateTime clock;
+        private Stack<string> urlhistory = new Stack<string>();
+        private string poweraltoversion = "2.0";
 
-    public HttpQueryReturnObject HttpQuery(string Url, bool AsXml = true) {
-      // this works. there's some logic missing from the original powershell version of this
-      // that may or may not be important (it was error handling of some flavor)
-      // also, all requests should not be treated as XML for this to be more generic
-      // (the powershell version had an "-asxml" flag to handle this)
+        public string Device { get; set; }
+        public int Port { get; set; }
+        public string ApiKey { get; set; }
+        public string Protocol { get; set; }
 
-      HttpWebResponse Response = null;
-      HttpStatusCode StatusCode = new HttpStatusCode();
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Serial { get; set; }
 
-      try {
-        HttpWebRequest Request = WebRequest.Create(Url) as HttpWebRequest;
+        public string OsVersion { get; set; }
+        public string GpAgent { get; set; }
+        public string AppVersion { get; set; }
+        public string ThreatVersion { get; set; }
+        public string WildFireVersion { get; set; }
+        public string UrlVersion { get; set; }
+        public XmlDocument LastXmlResult { get; set; }
 
-        //if (Response.ContentLength > 0) {
-
-        try {
-          Response = Request.GetResponse() as HttpWebResponse;
-          StatusCode = Response.StatusCode;
-        } catch (WebException we) {
-          StatusCode = ((HttpWebResponse)we.Response).StatusCode;
+        public string ApiUrl {
+            get {
+                if ( !string.IsNullOrEmpty( this.Protocol ) && !string.IsNullOrEmpty( this.Device ) && this.Port > 0 ) {
+                    return this.Protocol + "://" + this.Device + ":" + this.Port + "/api/";
+                } else {
+                    return null;
+                }
+            }
         }
 
-        string DetailedError = Response.GetResponseHeader("X-Detailed-Error");
-         // }
-
-      } catch {
-        throw new HttpException("httperror");
-      }
-
-      if (Response.StatusCode.ToString() == "OK") {
-        StreamReader Reader = new StreamReader(Response.GetResponseStream());
-        string Result = Reader.ReadToEnd();
-        XmlDocument XResult = new XmlDocument();
-
-        if (AsXml) {
-          XResult.LoadXml(Result);
+        public string AuthString {
+            get {
+                if ( !string.IsNullOrEmpty( this.ApiKey ) ) {
+                    return "key=" + this.ApiKey;
+                } else {
+                    return null;
+                }
+            }
         }
 
-        Reader.Close();
-        Response.Close();
 
-        HttpQueryReturnObject ReturnObject = new HttpQueryReturnObject();
-        ReturnObject.Statuscode = StatusCode;
-        if (AsXml) { ReturnObject.Data = XResult; }
-        ReturnObject.RawData = Result;
-        
-        this.rawqueryhistory.Push(Result);
-        //this.queryhistory.Push(XResult);
+        public string PowerAltoVersion {
+            get {
+                return this.poweraltoversion;
+            }
+        }
 
-        return ReturnObject;
+        public string Clock {
+            get {
+                return this.clock.ToString();
+            }
+            set {
+                this.clock = DateTime.Parse( value );
+            }
+        }
+        public DateTime ClockasDateTime {
+            get {
+                return this.clock;
+            }
+            set {
+                this.clock = value;
+            }
+        }
 
-      } else {
+        public string[] UrlHistory {
+            get {
+                return this.urlhistory.ToArray();
+            }
+        }
 
-        throw new HttpException("httperror");
-      }
-    }		
-	}
+        public void FlushHistory() {
+            this.urlhistory.Clear();
+        }
+
+
+        public string UrlBuilder(string QueryString) {
+
+            string[] Pieces = new string[5];
+            Pieces[0] = this.ApiUrl;
+            Pieces[1] = "?";
+            Pieces[2] = this.AuthString;
+            Pieces[3] = "&";
+            Pieces[4] = QueryString;
+
+            //if (QueryType == "op") {
+            //  Pieces[5] += ("&cmd=" + Query);
+            //}
+
+            string CompletedString = string.Join( "", Pieces );
+
+            this.urlhistory.Push( CompletedString );
+            return CompletedString;
+        }
+
+        private static bool OnValidateCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+            return true;
+        }
+
+        public void OverrideValidation() {
+            ServicePointManager.ServerCertificateValidationCallback = OnValidateCertificate;
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+        }
+
+        //Holds the raw result of the last query
+        //Would like to convert this to emulate UrlHistory, but I think we need to get the HttpQuery helper as a method to PaDevice first
+
+        private Stack<string> rawqueryhistory = new Stack<string>();
+
+        public string[] RawQueryHistory {
+            get {
+                return this.rawqueryhistory.ToArray();
+            }
+        }
+
+        public void FlushRawQueryHistory() {
+            this.rawqueryhistory.Clear();
+        }
+
+        /*
+        private Stack<XmlDocument> queryhistory = new Stack<XmlDocument>();
+
+        public string[] QueryHistory {
+          get {
+            return this.queryhistory.ToArray();
+          }
+        }
+    
+        public void FlushQueryHistory () {
+          this.queryhistory.Clear();
+        }
+        */
+
+        public HttpQueryReturnObject HttpQuery(string Url, bool AsXml = true) {
+            // this works. there's some logic missing from the original powershell version of this
+            // that may or may not be important (it was error handling of some flavor)
+            // also, all requests should not be treated as XML for this to be more generic
+            // (the powershell version had an "-asxml" flag to handle this)
+
+            HttpWebResponse Response = null;
+            HttpStatusCode StatusCode = new HttpStatusCode();
+
+            try {
+                HttpWebRequest Request = WebRequest.Create( Url ) as HttpWebRequest;
+
+                //if (Response.ContentLength > 0) {
+
+                try {
+                    Response = Request.GetResponse() as HttpWebResponse;
+                    StatusCode = Response.StatusCode;
+                } catch ( WebException we ) {
+                    StatusCode = ( (HttpWebResponse)we.Response ).StatusCode;
+                }
+
+                string DetailedError = Response.GetResponseHeader( "X-Detailed-Error" );
+                // }
+
+            } catch {
+                throw new HttpException( "httperror" );
+            }
+
+            if ( Response.StatusCode.ToString() == "OK" ) {
+                StreamReader Reader = new StreamReader( Response.GetResponseStream() );
+                string Result = Reader.ReadToEnd();
+                XmlDocument XResult = new XmlDocument();
+
+                if ( AsXml ) {
+                    XResult.LoadXml( Result );
+                }
+
+                Reader.Close();
+                Response.Close();
+
+                HttpQueryReturnObject ReturnObject = new HttpQueryReturnObject();
+                ReturnObject.Statuscode = StatusCode;
+                if ( AsXml ) { ReturnObject.Data = XResult; }
+                ReturnObject.RawData = Result;
+
+                this.rawqueryhistory.Push( Result );
+                //this.queryhistory.Push(XResult);
+
+                return ReturnObject;
+
+            } else {
+
+                throw new HttpException( "httperror" );
+            }
+        }
+    }
 }
