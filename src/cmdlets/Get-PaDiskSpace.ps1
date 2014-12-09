17a724583adbf096197d9df9beff5090143931a3
brianaddicks <brian@addicks.us>
2014-12-09 15:09:50 -0500

    added Get-PaInterfaceCounter

---
+++ src/cmdlets/Get-PaDiskSpace.ps1
@@ -0,0 +1,42 @@
+﻿function Get-PaDiskSpace {
+    [CmdletBinding()]
+    <#Param (
+        [Parameter(Mandatory=$True,Position=0)]
+        [ValidatePattern('^(ethernet\d+\/\d+(\.\d+)?|(loopback|tunnel|vlan)(\.\d+)?)$')]
+        [string]$Name
+    )#>
+
+    $Command = "<show><system><disk-space></disk-space></system></show>"
+
+    $ResponseData = Invoke-PaOperation $Command
+    $Global:test = $ResponseData
+
+    $ResponseSplit = $ResponseData.'#cdata-section'.Split("`r`n")
+    
+    $OutputRx = [regex] '(?msx)
+                         (?<filesystem>[a-z0-9\/]+)\ +
+                         (?<size>[0-9\.A-Z]+)\ +
+                         (?<used>[0-9\.A-Z]+)\ +
+                         (?<available>[0-9\.A-Z]+)\ +
+                         (?<percent>\d+%)\ +
+                         (?<mount>[\/a-z]+)
+                         '
+    $ReturnObjects = @()
+
+    foreach ($r in $ResponseSplit) {
+        $Match = $OutputRx.Match($r)
+        if ($Match.Success) {
+            $ReturnObject             = "" | Select FileSystem,Size,Used,Available,PercentUsed,MountPoint
+            $ReturnObject.FileSystem  = $Match.Groups['filesystem'].Value
+            $ReturnObject.Size        = $Match.Groups['size'].Value
+            $ReturnObject.Used        = $Match.Groups['used'].Value
+            $ReturnObject.Available   = $Match.Groups['available'].Value
+            $ReturnObject.PercentUsed = $Match.Groups['percent'].Value
+            $ReturnObject.MountPoint  = $Match.Groups['mount'].Value
+            
+            $ReturnObjects += $ReturnObject
+        }
+    }
+
+    return $ReturnObjects
+}
---
+++ src/cmdlets/Get-PaInterfaceCounter.ps1
@@ -0,0 +1,45 @@
+﻿function Get-PaInterfaceCounter {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True,Position=0)]
+        [ValidatePattern('^(ethernet\d+\/\d+(\.\d+)?|(loopback|tunnel|vlan)(\.\d+)?)$')]
+        [string]$Name
+    )
+
+    if ($Ethernet -or $Loopback -or $Vlan -or $Tunnel) {
+        $TypeSpecified = $True
+    }
+
+    if ($Name) {
+        $Command = "<show><counter><interface>$Name</interface></counter></show>"
+    } else {
+        $Command = "<show><counter><interface>all</interface></counter></show>"
+    }
+
+    $ResponseData = Invoke-PaOperation $Command
+    $Global:test = $ResponseData
+
+    function ProcessInterface ($entry,$hw) {
+        $interfaceObject = New-Object PowerAlto.InterfaceStatus
+        
+        if ($hw) {
+            Write-Verbose "hw found"
+
+            $interfaceObject.InBytes  = $hw.entry.ibytes
+            $interfaceObject.OutBytes = $hw.entry.obytes
+            $interfaceObject.InDrops  = $hw.entry.idrops
+            $interfaceObject.InErrors = $hw.entry.ierrors
+        } else {
+            $interfaceObject.InBytes  = $entry.ifnet.entry.ibytes
+            $interfaceObject.OutBytes = $entry.ifnet.entry.obytes
+            $interfaceObject.InDrops  = $entry.ifnet.entry.idrops
+            $interfaceObject.InErrors = $entry.ifnet.entry.ierrors
+        }
+
+        $interfaceObject.Name          = $entry.ifnet.entry.name
+        
+        return $interfaceObject
+    }
+
+    return ProcessInterface $ResponseData.ifnet $ResponseData.hw
+}
