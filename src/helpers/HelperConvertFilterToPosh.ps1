187e3e7dc4fc3147306fdbfb821297c509e04328
Brian Addicks <brian@addicks.us>
2015-04-21 12:51:34 -0400

    added helper function for converting dynamic group filters to posh

---
+++ src/helpers/HelperConvertFilterToPosh.ps1
@@ -0,0 +1,28 @@
+﻿function HelperConvertFilterToPosh {
+    [CmdletBinding()]
+    Param (
+		[Parameter(Mandatory=$True,Position=0)]
+		[string]$Filter,
+
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]$VariableName,
+
+        [Parameter(Mandatory=$True,Position=2)]
+        [string]$Property
+    )
+
+    $FilterSplit = $Filter.Split()
+
+    $MatchString = "`$$VariableName | ? { "
+    foreach ($f in $FilterSplit) {
+        switch ($f) {
+            { $_ -match '^(and|or)$' } { $MatchString += " -$f " }
+                               default { $MatchString += "( `$_.$Property -contains $f )" }
+        }
+    }
+    $MatchString += " }"
+
+    return $MatchString
+}
+
+HelperConvertFilterToPosh "'internal' and 'github' or 'tag1'" Address Tags | clip
