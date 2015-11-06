7b97c3e589d8e7f99345194442c44be439f30f4d
Brian Addicks <brian@addicks.us>
2015-11-05 20:46:49 -0500

    updates type/format file for authenticationprofile

--- buildmodule.ps1
+++ buildmodule.ps1
@@ -65,8 +65,8 @@ $ManifestParams = @{ Path = $ManifestFile
                      Author             = 'Brian Addicks'
                      RootModule         = 'PowerAlto2.psm1'
                      PowerShellVersion  = '4.0'
-                     TypesToProcess     = @('poweralto.types.ps1xml') }
-                     #FormatsToProcess   = @('poweralto.format.ps1xml') }
+                     TypesToProcess     = @('poweralto.types.ps1xml')
+                     FormatsToProcess   = @('poweralto.format.ps1xml') }
 
 New-ModuleManifest @ManifestParams
 
