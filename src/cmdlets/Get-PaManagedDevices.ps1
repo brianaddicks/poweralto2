0e9b87d5c1cfaa79aef3d135241ec04ba92e6b95
Brian Addicks <brian@addicks.us>
2016-09-08 10:13:26 -0400

    added Get-PaManagedDevices

---
+++ src/cmdlets/Get-PaManagedDevices.ps1
@@ -0,0 +1,29 @@
+function Get-PaManagedDevices {
+    [CmdletBinding()]
+    Param (
+    )
+
+    $Command = "<show><devices><all></all></devices></show>"
+
+    $ResponseData = Invoke-PaOperation $Command
+
+    $ResponseTable = @()
+
+    foreach ($r in $ResponseData.devices.entry) {
+        $ResponseObject = New-Object PowerAlto.PaDevice
+
+        $ResponseObject.Name            = $r.hostname
+        $ResponseObject.Model           = $r.model
+        $ResponseObject.Serial          = $r.serial
+        $ResponseObject.OsVersion       = $r.'sw-version'
+        $ResponseObject.GpAgent         = $r.'global-protect-client-package-version'
+        $ResponseObject.AppVersion      = $r.'app-version'
+        $ResponseObject.ThreatVersion   = $r.'threat-version'
+        $ResponseObject.WildFireVersion = $r.'wildfire-version'
+        $ResponseObject.UrlVersion      = $r.'url-filtering-version'
+
+        $ResponseTable                += $ResponseObject
+    }
+
+    return $ResponseTable
+}
